263796168c1c36a97f07677027d5845e23003a6a
Abhishek Biswas <abhishekbiswas876@gmail.com>
2025-05-15 00:54:46 +0530

    Some changes

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,14 +1,16 @@
+import Message from "../models/message.model.js";
+
 export const handleSocket = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
-    // socket.on("sendMessage", async ({ senderId, text }) => {
-    //   const message = new Message({ senderId, text });
-    //   await message.save();
-    //   const fullMessage = await message.populate("senderId", "username");
+    socket.on("sendMessage", async ({ senderId, text }) => {
+      const message = new Message({ senderId, text });
+      await message.save();
+      const fullMessage = await message.populate("senderId", "username");
 
-    //   io.emit("newMessage", fullMessage); // broadcast to all
-    // });
+      io.emit("newMessage", fullMessage); // broadcast to all
+    });
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Message from "../models/message.model.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { handleSocket } from "../lib/socket.js"; // Import the socket instance
+// Import the socket instance
 
 const router = express.Router();
 
@@ -10,25 +10,6 @@ router.get("/", protectRoute, async (req, res) => {
     .populate("senderId", "username")
     .sort({ createdAt: 1 });
   res.json(messages);
-});
-
-router.post("/", protectRoute, async (req, res) => {
-  const { text, emoji } = req.body;
-  const { userId } = req.user;
-
-  const message = new Message({
-    senderId: userId,
-    text,
-    emoji,
-  });
-
-  await message.save();
-  const fullMessage = await message.populate("senderId", "username");
-
-  // Emit the message to all connected clients
-  handleSocket.emit("newMessage", fullMessage); // Emit the message to all connected clients using the socket instance
-
-  res.status(201).json(fullMessage);
 });
 
 export default router;
