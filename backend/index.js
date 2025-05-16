231a411a10f5187b0b57be0dc5693daffb6a4b47
Abhishek Biswas <abhishekbiswas876@gmail.com>
2025-05-16 17:02:25 +0530

    Commit before production

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { Server } from "socket.io";
@@ -12,6 +13,7 @@ import { handleSocket } from "./lib/socket.js";
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
+const __dirname = path.resolve();
 
 const io = new Server(server, {
   cors: { origin: "http://localhost:5173", credentials: true },
@@ -19,11 +21,21 @@ const io = new Server(server, {
 
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/messages", messageRoutes);
 
 handleSocket(io); // Socket logic
+
+//deployment
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 mongoose
   .connect(process.env.MONGO_URI)
--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, //prevent xss attacks , cross site scripting attacks
-    sameSite: "Lax", // csrf attacks cross site request forgery attacks
-    secure: process.env.NODE_ENV == "development",
+    sameSite: "strict", // csrf attacks cross site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
   });
   return token;
 };
